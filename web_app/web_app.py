e309e50dfc4d7e519a002c1da4f47071b2ef1b85
Thiago Cordeiro <thiago6891@gmail.com>
2018-03-21 18:45:19 -0300

    Adding first URL to Redis. Resolves #3

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-from redis import Redis
+from redis import Redis, RedisError
+
+URLS_TO_VISIT_KEY = 'urls_to_visit'
+STARTING_PAGE_URL = 'http://www.epocacosmeticos.com.br/'
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
@@ -8,10 +11,17 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     html = '<h1>Server Running</h1>'
     return html
 
 
 if __name__ == "__main__":
+    try:
+        if redis.scard(URLS_TO_VISIT_KEY) == 0:
+            redis.sadd(URLS_TO_VISIT_KEY, STARTING_PAGE_URL)
+    except RedisError:
+        # Redis didn't connect, let the application crash
+        # and Docker will handle reinitialization
+        raise
     app.run(host='0.0.0.0', port=80)
